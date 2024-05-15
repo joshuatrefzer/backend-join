528ddb892e4fb650638c94f17ccf470f66ddce49
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-05-15 16:11:54 +0200

    allowed hosts

--- backend_join/backend_join/settings.py
+++ backend_join/backend_join/settings.py
@@ -42,7 +42,8 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'http://localhost:4200',
-    'http://127.0.0.1:4200'
+    'http://127.0.0.1:4200',
+    'joshuatrefzer.pythonanywhere.com'
 ]
 
 
